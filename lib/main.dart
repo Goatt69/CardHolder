c6e2559be84aaa2e67eb356c2d114e2bec4a607d
Quyet14 <tranquyetz098@gmail.com>
2024-12-17 19:18:24 +0700

    đăng nhập và đăng xuất

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
+import 'package:cardholder/login_page.dart';
 import 'package:flutter/material.dart';
-
+import 'login_page.dart';
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -30,8 +32,11 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
